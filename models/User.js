e7d62ac5a58161cf66f63b7c9e853726a6299799
Nic Vill <nicolas.villeneuve.01@gmail.com>
2022-07-24 07:47:38 -0400

    register endpoint configured

--- models/User.js
+++ models/User.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import isEmail from "validator/lib/isEmail.js";
 
 const UserSchema = new mongoose.Schema({
     name: {type: String, required: [true, 'Please provide name'], minLength: 3, maxLength: 20, trim: true},
-    email: {type: String, required: [true, 'Please provide email'], unique: true},
-    password: {type: String, required: [true, 'Please provide password'], minLength: 6},
+    email: {type: String, required: [true, 'Please provide email'], validate: {
+        validator: isEmail, message: "Please provide a valid email"
+        }, unique: true},
+    password: {type: String, required: [true, 'Please provide password'], minLength: 3},
     lastName: {type: String, maxLength: 20, trim: true, default: "lastName"},
     location: {type: String, trim: true, maxLength: 20, default: "my city"}
 });
