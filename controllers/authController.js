1761bb739c182f13fc2639d66c3dbfa8e0284a84
Nic Vill <nicolas.villeneuve.01@gmail.com>
2022-07-25 09:10:49 -0400

    error handling for api functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,22 @@
 import User from "../models/User.js";
 import {StatusCodes} from "http-status-codes";
 
-const register = async (req, res) => {
 
-    const user = await User.create(req.body);
+class CustomAPIError extends Error{
+    constructor(message){
+        super(message);
+        this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
+
+const register = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    if (!name || !email || !password) {
+        throw new CustomAPIError("please provide all values");
+    }
+
+    const user = await User.create({name, email, password});
     res.status(StatusCodes.OK).json({user});
 
 };
