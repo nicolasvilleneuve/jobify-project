0cc7a2eb6a1d17554e6e888a26ce0aa2ccd85f13
Nic Vill <nicolas.villeneuve.01@gmail.com>
2022-07-28 14:06:08 -0400

    added nested pages and protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,31 @@ import {
     Route
 } from "react-router-dom";
 import styled from 'styled-components';
-import {Landing, Error, Register} from "../src/pages";
+import {Landing, Error, Register, ProtectedRoute} from "../src/pages";
 import {AddJob, AllJobs, Profile, Stats, SharedLayout} from "./pages/dashboard/index.js";
 
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<SharedLayout />}>
-                  <Route path="stats" element={<Stats />}/>
-                  <Route path="all-jobs" element={<AllJobs />}/>
-                  <Route path="add-job" element={<AddJob />}/>
-                  <Route path="profile" element={<Profile />}/>
-              </Route>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={
+                    <ProtectedRoute>
+                        <SharedLayout/>
+                    </ProtectedRoute>
+                }>
+                    <Route index element={<Stats/>}/>
+                    <Route path="all-jobs" element={<AllJobs/>}/>
+                    <Route path="add-job" element={<AddJob/>}/>
+                    <Route path="profile" element={<Profile/>}/>
+                </Route>
 
-              <Route path="/landing" element={<Landing />}/>
-              <Route path="/register" element={<Register />}/>
-              <Route path="*" element={<Error />}/>
-          </Routes>
-      </BrowserRouter>
-  );
+                <Route path="/landing" element={<Landing/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="*" element={<Error/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
