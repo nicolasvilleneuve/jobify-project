f8cb8812181398b87e2b78f32999992cc2f1d3c2
Nic Vill <nicolas.villeneuve.01@gmail.com>
2022-07-29 10:42:03 -0400

    big sidebar complete

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,15 +1,25 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
 import {Logo, NavLinks} from "./index";
+import {useAppContext} from "../context/appContext";
+
+
 
 const BigSidebar = () => {
+    const {showSidebar} = useAppContext();
+
     return (
         <Wrapper>
-            <div>
-                <header>
-                <Logo/>
-                </header>
+            <div className={
+                showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+                }
+            >
+                <div className="content">
+                    <header>
+                        <Logo/>
+                    </header>
+                    <NavLinks />
+                </div>
             </div>
-            <NavLinks />
         </Wrapper>
     )
 };
