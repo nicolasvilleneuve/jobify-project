65cf7158e8cea5d54a64768c309d43f131989872
Nic Vill <nicolas.villeneuve.01@gmail.com>
2022-07-29 11:46:22 -0400

    auth middleware complete

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,22 @@
+import {UnauthenticatedError} from "../errors/index.js";
+import jwt from "jsonwebtoken";
+
+
 const auth = async (req, res, next) => {
-    console.log('authenticate user');
-    next();
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new UnauthenticatedError("Authentication invalid")
+    }
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = payload;
+        next();
+    } catch (error) {
+        throw new UnauthenticatedError('Authentication invalid');
+    }
 };
 
 export default auth;
