b3bf7b0f99cb2c72f0b7a95c7957d75d6c67633b
Nic Vill <nicolas.villenueve.01@gmail.com>
2022-08-19 11:37:49 -0400

    bar chart added to stats page

---
+++ client/src/components/AreaChart.js
@@ -0,0 +1,9 @@
+const AreaChartComponent = () => {
+    return (
+        <div>
+            <h2> Area chart</h2>
+        </div>
+    )
+};
+
+export default AreaChartComponent;
--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,8 +1,21 @@
+import {useEffect, useState} from 'react';
+import BarChartComponent from "./BarChart";
+import AreaChartComponent from "./AreaChart";
+import {useAppContext} from "../context/appContext";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+
+
 const ChartsContainer = () => {
+    const [barChart, setBarChart] = useState(true);
+    const {monthlyApplications: data} = useAppContext();
+
+
     return (
-        <div>
-            <h5> This is a chart area</h5>
-        </div>
+        <Wrapper>
+            <h4> Monthly Applications</h4>
+            <button type='button' onClick={()=> setBarChart(!barChart)}>{barChart ? 'AreaChart' : 'BarChart'}</button>
+            {barChart ? <BarChartComponent data={data}/> : <AreaChartComponent data={data}/>}
+        </Wrapper>
     )
 };
 
