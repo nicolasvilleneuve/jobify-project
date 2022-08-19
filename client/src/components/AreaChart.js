e7c4bddcc57ca4d31c21f28f9481a94de166240f
Nic Vill <nicolas.villenueve.01@gmail.com>
2022-08-19 11:41:08 -0400

    area chart added

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,8 +1,17 @@
-const AreaChartComponent = () => {
+import React from 'react';
+import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, BarChart, Bar} from "recharts";
+
+const AreaChartComponent = ({data}) => {
     return (
-        <div>
-            <h2> Area chart</h2>
-        </div>
+        <ResponsiveContainer width='100%' height={300}>
+        <AreaChart data={data} margin={{top:50}}>
+            <CartesianGrid strokeDasharray='3 3'/>
+            <XAxis dataKey='date' />
+            <YAxis allowDecimals={false} />
+            <Tooltip />
+            <Area dataKey='count' fill='#2cb1bc' stroke="#2cb1bc" type="monotone"/>
+        </AreaChart>
+    </ResponsiveContainer>
     )
 };
 
